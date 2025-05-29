be09078404751783cca06daf33b95f70216fa616
Neeha Ravula <ravulaneeha@gmail.com>
2025-05-28 21:24:34 -0700

    added web footer

---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+{
+  /* Component UI: Footer */
+}
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="w-full py-8 px-8">
+      <div className="flex justify-between items-center text-sm font-[family-name:var(--font-geist-mono)]">
+        <div>© 2025</div>
+        <div>About</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/general-layout.tsx
@@ -0,0 +1,28 @@
+{
+  /* Component Layout: lays out nav and footer */
+}
+
+import Footer from "@/components/footer";
+
+export interface LayoutConfig {
+  variant: "home" | "work" | "project" | "play";
+}
+
+type LayoutProps = {
+  children: React.ReactNode;
+  variant: LayoutConfig["variant"];
+};
+
+const Layout = ({ children, variant }: LayoutProps) => {
+  const showFooter = variant !== "project";
+
+  return (
+    <div className="flex flex-col min-h-screen h-screen overflow-hidden">
+      <main className="flex-1 overflow-auto p-8 sm:p-20">{children}</main>
+
+      {showFooter && <Footer />}
+    </div>
+  );
+};
+
+export default Layout;
