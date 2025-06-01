85aa8967b9838d04784165b52b7f92f36186a02a
Neeha Ravula <ravulaneeha@gmail.com>
2025-05-31 19:16:49 -0700

    updated image gallery to use s3 and masonry layout

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nravula-portfolio-assets.s3.amazonaws.com",
+        pathname: "/**", // Match all paths under your S3 bucket
+      },
+    ],
+  },
 };
 
 export default nextConfig;
