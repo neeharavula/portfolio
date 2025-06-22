5a86c1cd5f14077ff6c55e876cc8c75e891d389a
Neeha Ravula <ravulaneeha@gmail.com>
2025-06-22 15:29:21 -0700

    updated h1 position

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <section className="flex flex-col justify-between h-full w-full px-8">
         {/* Large summary text */}
         <div className="flex-1 flex items-center">
-          <h1 className="font-offbit text-[3rem] md:text-[4em] lg:text-[4rem] xl:text-[6rem] leading-none">
+          <h1 className="font-offbit mt-25 text-[3rem] md:text-[4em] lg:text-[4rem] xl:text-[6rem] leading-none">
             {/* Example summary */}
             Neeha Ravula is a developer <br></br>based in San Diego, CA,
             exploring <br></br> the intersection of design and <br></br>
