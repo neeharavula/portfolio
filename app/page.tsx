1ade31f91f94903c2701f20a3e1de34e62d41e4b
Neeha Ravula <ravulaneeha@gmail.com>
2025-06-22 16:05:44 -0700

    added text loop feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/base/general-layout";
+import { TextLoop } from "@/components/motion-primitives/text-loop";
 import Link from "next/link";
 
 export default function Home() {
@@ -7,10 +8,42 @@ export default function Home() {
       <section className="flex flex-col justify-between h-full w-full px-8">
         {/* Large summary text */}
         <div className="flex-1 flex items-center">
-          <h1 className="font-offbit mt-20 text-[3rem] md:text-[5em] lg:text-[5rem] xl:text-[6rem] leading-none">
-            {/* Example summary */}
-            Neeha Ravula is a developer <br></br>based in San Diego, CA,
-            exploring <br></br> the intersection of design and <br></br>
+          <h1 className="font-offbit mt-20 text-[4rem] md:text-[5em] lg:text-[5rem] xl:text-[6rem] leading-none">
+            Neeha Ravula is a{" "}
+            <TextLoop
+              className="overflow-y-clip text-[#94A75D]"
+              transition={{
+                type: "spring",
+                stiffness: 900,
+                damping: 80,
+                mass: 10,
+              }}
+              variants={{
+                initial: {
+                  y: 20,
+                  rotateX: 90,
+                  opacity: 0,
+                  filter: "blur(4px)",
+                },
+                animate: {
+                  y: 0,
+                  rotateX: 0,
+                  opacity: 1,
+                  filter: "blur(0px)",
+                },
+                exit: {
+                  y: -20,
+                  rotateX: -90,
+                  opacity: 0,
+                  filter: "blur(4px)",
+                },
+              }}
+            >
+              <span>developer</span>
+              <span>creative</span>
+            </TextLoop>
+            <br></br>based in San Diego, CA, exploring <br></br> the
+            intersection of design and <br></br>
             computation.
           </h1>
         </div>
