649d0ae82d2033cc47f90f0902e5e1ef18edd069
Neeha Ravula <ravulaneeha@gmail.com>
2025-12-19 16:30:46 -0800

    updated image version

--- config/image-version.ts
+++ config/image-version.ts
@@ -2,4 +2,4 @@
 
 // Increment this version number whenever you update images in Vercel Blob
 // Forces browsers to fetch new images instead of using cached versions
-export const IMAGE_VERSION = "v1";
+export const IMAGE_VERSION = "v2";
