838a393be57b1149943050c9112004216191cf75
Neeha Ravula <ravulaneeha@gmail.com>
2026-09-21 20:58:08 -0400

    updated mobile styles

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -34,7 +34,7 @@ export default function Play() {
         }));
 
   return (
-    <main className="flex-1 flex flex-col md:flex-row gap-y-lg md:gap-x-8 px-16 pt-md md:pt-16 pb-xl font-content text-sm">
+    <main className="flex-1 flex flex-col md:flex-row gap-y-lg md:gap-x-8 px-xl md:px-16 pt-md md:pt-16 pb-xl font-content text-sm">
       {/* Sidebar */}
       <div className="w-full md:w-1/4 md:shrink-0 space-y-10">
         <div>
@@ -44,7 +44,7 @@ export default function Play() {
           <p className="text-tertiary mt-sm">Explorations in other mediums</p>
         </div>
 
-        <nav className="flex flex-col gap-sm font-navigation text-sm uppercase">
+        <nav className="hidden md:flex flex-col gap-sm font-navigation text-sm uppercase">
           {filterOptions.map((filter) => (
             <button
               key={filter}
